a546f5dd1808edfee9b7110269fc771817767f94
Pvvirg <pvvirg16@gmail.com>
2024-04-01 13:20:41 -0300

    Vídeo incorporação

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,16 @@ export default function Home() {
   return (
     <main>
       <CarrosselBanner />
-
+      <iframe
+        width="903"
+        height="742"
+        src="https://www.youtube.com/embed/nDdOivSBMY4"
+        title="Máquina de Gerar Empregos   Aula 01"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
       <NewsHome />
       <BannerHome />
       <Newsletter />
